ce9f55e26ae0cc6b3e22cc3798c1623fb686cce2
RafaPalau <rafaeldonizetip@gmail.com>
2021-05-21 12:03:07 -0300

    carousel resposivity

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -2,6 +2,7 @@ import ReactSlidy from "react-slidy";
 import CardsBlog from "../CardsBlog";
 import styles from "./styles.module.scss";
 import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
+import useuseWindowDimensions from "../../hook/useWindowDimensions";
 
 function CustomArrow({ children, className, ...props }) {
   return (
@@ -30,6 +31,26 @@ function CustomArrowRight(props) {
 }
 
 export default function Carousel() {
+  const { width } = useuseWindowDimensions();
+  let numberCardsOfSlide ;
+
+  if ( width <= 314) {
+    numberCardsOfSlide = 1;
+  }
+  else if (width >= 315 && width <= 420) {
+    numberCardsOfSlide = 1.3;
+  } else if (width >= 421 && width <= 499) {
+    numberCardsOfSlide = 2;
+  } else if (width >= 500 && width <= 599) {
+    numberCardsOfSlide = 2.5;
+  } else if (width >= 600 && width <= 699) {
+    numberCardsOfSlide = 2.8;
+  } else if (width >= 699 && width <= 799) {
+    numberCardsOfSlide = 3;
+  } else {
+    numberCardsOfSlide = 3.3;
+  } 
+
   return (
     <div className={styles.container}>
       <h1>Read Our Blog</h1>
@@ -38,44 +59,47 @@ export default function Carousel() {
         Vokalia and Consonantia, there live the blind texts.
       </p>
 
-      <ReactSlidy
-        ArrowLeft={CustomArrowLeft}
-        ArrowRight={CustomArrowRight}
-        infiniteLoop
-        className={styles.carousel}
-        numOfSlides={3.3}
-      >
-        <CardsBlog
-          image='/images/bloco_image_1.svg'
-          alt=''
-          title='Quick-start guide to nuts and seeds'
-          avatar='/images/user1.jpg'
-          name='Michael Dam'
-        />
-        <CardsBlog
-          image='/images/bloco_image_2.svg'
-          alt=''
-          title='Nutrition: Tips for Improving Your Health'
-          avatar='/images/user2.jpg'
-          name='Ben Parker'
-        />
+      <div className={styles.carousel}>
+        <ReactSlidy  
+          ArrowLeft={CustomArrowLeft}
+          ArrowRight={CustomArrowRight}
+          infiniteLoop
+          
+          className={styles.cardscarousel}
+          numOfSlides={numberCardsOfSlide}
+        >
+          <CardsBlog
+            image='/images/bloco_image_1.svg'
+            alt=''
+            title='Quick-start guide to nuts and seeds'
+            avatar='/images/user1.jpg'
+            name='Michael Dam'
+          />
+          <CardsBlog
+            image='/images/bloco_image_2.svg'
+            alt=''
+            title='Nutrition: Tips for Improving Your Health'
+            avatar='/images/user2.jpg'
+            name='Ben Parker'
+          />
 
-        <CardsBlog
-          image='/images/bloco_image_3.svg'
-          alt=''
-          title='The top 10 benefits of eating helathy'
-          avatar='/images/user3.jpg'
-          name='Luisa Villasmil'
-        />
+          <CardsBlog
+            image='/images/bloco_image_3.svg'
+            alt=''
+            title='The top 10 benefits of eating helathy'
+            avatar='/images/user3.jpg'
+            name='Luisa Villasmil'
+          />
 
-        <CardsBlog
-          image='/images/bloco_image_4.svg'
-          alt='Image'
-          title="Why did eating well made you live longer?"
-          avatar='/images/user4.jpg'
-          name='Vicky Hladynets'
-        />
-      </ReactSlidy>
+          <CardsBlog
+            image='/images/bloco_image_4.svg'
+            alt='Image'
+            title='Why did eating well made you live longer?'
+            avatar='/images/user4.jpg'
+            name='Vicky Hladynets'
+          />
+        </ReactSlidy>
+      </div>
     </div>
   );
 }
